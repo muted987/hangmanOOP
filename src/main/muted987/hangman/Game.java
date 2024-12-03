44b8a97aec9a6f38a103010c96c6fd441f1733e2
SunSetS5S <akashi322wp@yandex.ru>
2024-12-03 17:05:56 +0300

    feat: добавление механики начала новой игры после завершения предыдущей

--- src/main/muted987/hangman/Game.java
+++ src/main/muted987/hangman/Game.java
@@ -7,54 +7,62 @@ import main.muted987.hangman.objects.Word;
 
 public class Game {
 
-    private final String language = GameStart.start();
-    private int amountOfInputs = 0;
-    private int amountOfMistake = 0;
-    private final Word word = new Word(language);
-    private  final ArrayOfHiddenWord starsArray = new ArrayOfHiddenWord(word.getWord());
-    private final ArrayOfIncorrectLetters arrayOfIncorrectLetters = new ArrayOfIncorrectLetters();
-    private final ArrayOfInputLetters arrayOfInputLetters = new ArrayOfInputLetters();
+    private static final String EXIT_CODE = "EXIT";
+    private static final String LINE_OF_UNDERLINES = "--------------------------------------------";
+    private static final String WORD_WAS = "word was = ";
+    private static final String WIN_MESSAGE = "YOU WIN!";
+    private static final String GAME_OVER_MESSAGE = "GAME OVER";
+    private static final String AMOUNT_OF_INPUT_LETTERS_CAPTION = "Amount of inputted letters = ";
+    private static final String AMOUNT_OF_MISTAKE_LETTERS_CAPTION = "Amount of mistake = ";
+
+    private static int amountOfInputs = 0;
+    private static int amountOfMistake = 0;
+    private static final ArrayOfIncorrectLetters arrayOfIncorrectLetters = new ArrayOfIncorrectLetters();
+    private static final ArrayOfInputLetters arrayOfInputLetters = new ArrayOfInputLetters();
     public void game() {
-        HangmanRender hangmanRender = new HangmanRender();
         while (true) {
-            if (amountOfMistake == 7) {
-                System.out.println("GAME OVER");
-                endOfGame();
-                break;
-            }
-            if (!starsArray.isAnyStar()) {
-                System.out.println("You win!");
-                endOfGame();
-                break;
-            }
-            hangmanRender.render(amountOfMistake);
-            arrayOfInputLetters.arrayOfInputLettersRender();
-            starsArray.hiddenWordArrayRender(starsArray);
-            String letterInput = Input.inputLetter();
-            amountOfInputs++;
-            if (arrayOfInputLetters.isLetterNotInArray(letterInput)) arrayOfInputLetters.addLetterToArray(letterInput);
-            if (word.getIndexOfLetter(letterInput) != -1) {
-                while (word.getIndexOfLetter(letterInput) >= 0) {
-                    starsArray.replaceStarToLetter(letterInput, word.getIndexOfLetter(letterInput));
-                    word.replaceLetterToStar(word.getIndexOfLetter(letterInput));
+            String language = GameStart.start();
+            if (language.equals(EXIT_CODE)) break;
+            Word word = new Word(language);
+            ArrayOfHiddenWord starsArray = new ArrayOfHiddenWord(word.getWord());
+            HangmanRender hangmanRender = new HangmanRender();
+            while (true) {
+                if (amountOfMistake == 7) {
+                    System.out.println(GAME_OVER_MESSAGE);
+                    endOfGame(word);
+                    break;
                 }
-            } else {
-                if (arrayOfIncorrectLetters.isLetterNotInArray(letterInput)) {
-                    System.out.println("Incorrect letter");
-                    arrayOfIncorrectLetters.addLetterToArray(letterInput);
+                if (!starsArray.isAnyStar()) {
+                    System.out.println(WIN_MESSAGE);
+                    endOfGame(word);
+                    break;
+                }
+                hangmanRender.render(amountOfMistake);
+                arrayOfInputLetters.arrayOfInputLettersRender();
+                starsArray.hiddenWordArrayRender(starsArray);
+                String letterInput = Input.inputLetter();
+                amountOfInputs++;
+                if (arrayOfInputLetters.isLetterNotInArray(letterInput)) arrayOfInputLetters.addLetterToArray(letterInput);
+                if (word.getIndexOfLetter(letterInput) != -1) {
+                    while (word.getIndexOfLetter(letterInput) >= 0) {
+                        starsArray.replaceStarToLetter(letterInput, word.getIndexOfLetter(letterInput));
+                        word.replaceLetterToStar(word.getIndexOfLetter(letterInput));
+                    }
                 } else {
-                    System.out.println("Letter is used before");
+                    arrayOfIncorrectLetters.addLetterToArray(letterInput);
+                    amountOfMistake++;
                 }
-                amountOfMistake++;
             }
         }
     }
-    public void endOfGame() {
-        System.out.println("word was = " + word.wordRender());
+    public void endOfGame(Word word) {
+        System.out.println(WORD_WAS + word.wordRender());
+        System.out.println(LINE_OF_UNDERLINES);
         arrayOfIncorrectLetters.arrayOfIncorrectLettersRender();
-        System.out.println("--------------------------------------------");
         arrayOfInputLetters.arrayOfInputLettersRender();
-        System.out.println("Amount of inputted letters = " + amountOfInputs);
-        System.out.println("Amount of mistake = " + amountOfMistake);
+        System.out.println(LINE_OF_UNDERLINES);
+        System.out.println(AMOUNT_OF_INPUT_LETTERS_CAPTION + amountOfInputs);
+        System.out.println(AMOUNT_OF_MISTAKE_LETTERS_CAPTION + amountOfMistake);
+        System.out.println(LINE_OF_UNDERLINES);
     }
 }
