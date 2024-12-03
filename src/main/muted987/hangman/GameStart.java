1f61a87249ea8f317c1656cdbc01a89636df15f0
SunSetS5S <akashi322wp@yandex.ru>
2024-12-03 16:16:24 +0300

    refactor: удаление магических чисел и создание констант для вывода текста

--- src/main/muted987/hangman/GameStart.java
+++ src/main/muted987/hangman/GameStart.java
@@ -1,23 +1,29 @@
 package main.muted987.hangman;
 
 public class GameStart {
-    private static String language = "Russian";
+    private static final String RUSSIAN_LANGUAGE_CAPTION = "Russian";
+    private static final String ENGLISH_LANGUAGE_CAPTION = "English";
+    private static final String GAME_EXIT_MESSAGE = "Shut down the game. Good luck!";
+    private static String language = RUSSIAN_LANGUAGE_CAPTION;
+    private static final String GAME_MAIN_MENU_CAPTION = "1. Start new game (current language is %s)\n2. Change game language (default Russian)\n3. Exit\n";
+    private static final String GAME_LANGUAGE_CHANGE_MENU_CAPTION = "Choose language\n1. Russian\n2. English\n3. Back";
+    private static final int GAME_MAIN_MENU_OPTION = 1;
+    private static final int GAME_LANGUAGE_CHANGE_MENU_OPTION = 2;
+    private static final int GAME_LANGUAGE_CHOOSE_OPTION = 2;
     public static String start() {
         while (true) {
-            String gameStartMessage = "1. Start new game (current language is %s)\n2. Change game language (default Russian)\n3. Exit\n";
-            System.out.printf(gameStartMessage, language);
+            System.out.printf(GAME_MAIN_MENU_CAPTION, language);
             int option = Input.inputOption();
-            if (option == 1) {
+            if (option == GAME_MAIN_MENU_OPTION) {
                 return language;
-            } else if (option == 2){
-                String languageChangeSettings = "Choose language\n1. Russian\n2. English\n3. Back";
-                System.out.println(languageChangeSettings);
+            } else if (option == GAME_LANGUAGE_CHANGE_MENU_OPTION){
+                System.out.println(GAME_LANGUAGE_CHANGE_MENU_CAPTION);
                 int languageOption = Input.inputOption();
-                if (languageOption == 2) {
-                    language = "English";
+                if (languageOption == GAME_LANGUAGE_CHOOSE_OPTION) {
+                    language = ENGLISH_LANGUAGE_CAPTION;
                 }
             } else {
-                System.out.println("Shut down the game. Good luck!");
+                System.out.println(GAME_EXIT_MESSAGE);
                 System.exit(1);
             }
         }
